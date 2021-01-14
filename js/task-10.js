a762a563cb1979bca64d30b674fc71c5938b0494
Ivan Khailo <jakom333@gmail.com>
2021-01-14 22:25:06 +0200

    update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -2,6 +2,7 @@ import users from './users.js';
 
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
+// ================== 1 ======================
 // const getSortedUniqueSkills = users => {
 //   const skills = users.reduce((allSkills, user) => {
 //     allSkills.push(...user.skills);
@@ -14,12 +15,14 @@ import users from './users.js';
 // };
 // console.log(getSortedUniqueSkills(users));
 
+// =================== 2 =======================
 // const getSortedUniqueSkills = users => {
 //   const skills = users.flatMap(user => user.skills);
 //   return [...new Set(skills)].sort();
 // };
 // console.log(getSortedUniqueSkills(users));
 
+// ==================== 3 =======================
 const getSortedUniqueSkills = users =>
   [...new Set(users.flatMap(user => user.skills))].sort();
 console.log(getSortedUniqueSkills(users));
