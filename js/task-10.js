f9674ac489c3d5d012077546219c170f80860a39
Ivan Khailo <jakom333@gmail.com>
2021-01-14 19:22:11 +0200

    Обновил 10 задание

--- js/task-10.js
+++ js/task-10.js
@@ -3,6 +3,18 @@ import users from './users.js';
 // Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
 
 // const getSortedUniqueSkills = users => {
+//   const skills = users.reduce((allSkills, user) => {
+//     allSkills.push(...user.skills);
+//     return allSkills;
+//   }, []);
+//   let result = [];
+//   return skills
+//     .filter(skill => (!result.includes(skill) ? result.push(skill) : ''))
+//     .sort();
+// };
+// console.log(getSortedUniqueSkills(users));
+
+// const getSortedUniqueSkills = users => {
 //   const skills = users.flatMap(user => user.skills);
 //   return [...new Set(skills)].sort();
 // };
